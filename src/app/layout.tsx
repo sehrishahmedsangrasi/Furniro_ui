d93aa1efe4663b65c1bf6286f8c3ada100a606d2
sehrishahmedsangrasi <pc6210.sehrishahmed@gmail.com>
2025-01-27 23:13:01 +0500

    Furniro ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Poppins, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 import Navbar from '../components/navbar'; 
 import "./globals.css";
@@ -10,10 +10,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800","900"], 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export default function RootLayout({
   children,
